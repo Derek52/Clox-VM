6d2485fc70d52e7341848ec2e12cfcd5dd17aa09
Derek Briggs <contact@derekbriggs.xyz>
2022-12-28 17:40:16 -0500

    Finish implementing the scanner up to the end of chapter 16

--- include/vm.h
+++ include/vm.h
@@ -21,7 +21,9 @@ typedef enum {
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+
+InterpretResult interpret(const char* source);
+
 void push(Value value);
 Value pop();
 
