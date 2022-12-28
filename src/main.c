c29f8e0441c3aae164f9adc412961837a36c2bdc
Derek Briggs <contact@derekbriggs.xyz>
2022-12-27 23:02:20 -0500

    Finish chapter 14 by implementing basic structure of VM

--- src/main.c
+++ src/main.c
@@ -5,7 +5,12 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 17);
+    writeChunk(&chunk, constant, 17);
+
+    writeChunk(&chunk, OP_RETURN, 17);
 
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
